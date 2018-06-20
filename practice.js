f5495e49c9f8f374c9d4b113baf39371ae780bcb
swetha1121 <swethakm7@gmail.com>
2018-06-20 16:17:39 -0400

    add a new js file

---
+++ practice.js
@@ -0,0 +1,18 @@
+// var price = 15;
+// var money = 10;
+// if(money>=price){
+// console.log("buy item");
+// }
+// else{
+//     console.log("do not buy item");
+// }
+
+var weather = "rain";
+
+if (weather === "snow") {
+  console.log("Bring a coat.");
+} else if (weather === "rain") {
+  console.log("Bring a rain jacket.");
+} else {
+  console.log("Wear what you have on.");
+}
